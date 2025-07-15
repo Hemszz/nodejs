5443fa3247e735ec801b064523def88f89dffbcc
Hemanth <ihemanth@Hemanths-MacBook-Pro.local>
2025-07-16 00:05:13 +0530

    playing with GET POST PUT PATCH DELETE

--- app.js
+++ app.js
@@ -2,18 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send('Hello, Hemszz! Welcome to node.js with Express!');
+app.get("/hemszz", (req, res) => {
+  res.send({ firstName: "Hemszz", lastName: "Node.js" });
+});
+
+app.post("/hemszz", (req, res) => {
+  res.send({ message: "Post request received!" });
+});
+
+app.delete("/hemszz", (req, res) => {
+  res.send({ message: "Delete request received!" });
+});
+
+app.put("/hemszz", (req, res) => {
+  res.send({ message: "Put request received!" });
+});   
+
+app.patch("/hemszz", (req, res) => {
+  res.send({ message: "Patch request received!" });
 });
 
 app.use("/test", (req, res) => {
   res.send('This is a test route!');
 }); 
 
-app.use("/test2", (req, res) => {
-  res.send('This is another test route!');
-});
 
+app.use("/", (req, res) => {
+  res.send('Hello, Hemszz! Welcome to node.js with Express!');
+});
 
 app.listen(7777, () => {
   console.log('Server is running on port 7777');
