30519a07b72aaf9699541c8a7e7de1c9ffca9cb5
Hemszz <hemanthkumar.hemu18@gmail.com>
2025-08-16 08:45:31 +0530

    schema sanitization

--- app.js
+++ app.js
@@ -24,6 +24,74 @@ app.post('/signup', async (req, res) => {
   res.send('User signed up successfully'); 
 });
 
+app.get('/user', async (req, res) => {
+  const email = req.body.email;
+  try{
+    const users = await userModel.find({ email: email });
+    if(users.length === 0) {
+      res.status(404).send('User not found');
+    } else {
+      res.send(users);
+      console.log('User fetched successfully:', users);
+    }
+  }
+  catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).send('Internal Server Error');
+  }
+})
+
+app.get('/feed', async (req, res) => {
+  try{
+    const users = await userModel.find({ });
+    if(users.length === 0) {
+      res.status(404).send('User not found');
+    } else {
+      res.send(users);
+      console.log('Users fetched successfully:', users);
+    }
+  }
+  catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.delete('/user', async (req, res) => {
+  const userId = req.body.userId;
+  try{
+    const result = await userModel.findByIdAndDelete(userId);
+    if(result) {
+      res.send('User deleted successfully');
+      console.log('User deleted successfully:', result);
+    } else {
+      res.status(404).send('User not found');
+    }
+  }
+  catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.patch('/user', async (req, res) => {
+  const userId = req.body.userId;
+  const updateData = req.body;
+  try{
+    const result = await userModel.findByIdAndUpdate(userId, updateData, { new: true });
+    if(result) {
+      res.send('User updated successfully');
+      console.log('User updated successfully:', result);
+    } else {
+      res.status(404).send('User not found');
+    }
+  }
+  catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 connectDb().then(() => {
   console.log('Database connection established yayy!!');
   app.listen(7777, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
+    minlength: 2,
+    maxlength: 50,
   },
   email: {
     type: String,
@@ -12,29 +14,72 @@ const userSchema = new mongoose.Schema({
     unique: true,
     trim: true,
     lowercase: true,
+    //write a custom validator to check email format
+    validate(value) {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(value)) {
+        throw new Error('Invalid email format');
+      }
+    }
   },
   password: {
     type: String,
     required: true,
     minlength: 6,
+    //write a custom validator to check password strength
+    validate(value) {
+      if (!/[a-z]/.test(value) || !/[A-Z]/.test(value) || !/[0-9]/.test(value) || value.length < 6) {
+        throw new Error('Password must contain at least one lowercase letter, one uppercase letter, one number, and be at least 6 characters long');
+      }
+    }
   },
   age: {
     type: Number,
     required: true,
-    min: 0,
+    min: 18,
+    validate(value) {
+      if (value < 18) {
+        throw new Error('Age must be at least 18');
+      }
+    }
   },
   gender: {
     type: String,
+    validate(value) {
+      if(!["male", "female", "others"].includes(value)) {
+        throw new Error('Gender data not valid');
+      }
+    }
   },
   bio: {
     type: String,
     trim: true,
+    minlength: 10,
+    maxlength: 200
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
+  photoUrl: {
+    type: String,
+    default: 'https://example.com/default-profile.png',
+    validate(value) {
+      const urlRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/;
+      if (!urlRegex.test(value)) {
+        throw new Error('Invalid URL format');
+      }
+    }
   },
-}); 
+  skills: {
+    type: [String],
+    // Custom validation to ensure skills are not empty
+    validate(value) {
+      if (value.length === 0) {
+        throw new Error('Skills array cannot be empty');
+      }
+    }
+  }
+},
+{
+  timestamps: true,
+} ); 
 
 const userModel = mongoose.model('User', userSchema);
 
